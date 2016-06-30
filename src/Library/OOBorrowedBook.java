01ec81e4ef09c5c186f4981fdcb37cea8144396a
Yusuf Karim <karimy25@gmail.com>
2016-06-29 22:47:20 -0400

    Added getString and getByte to BorrowedBook

--- src/Library/OOBorrowedBook.java
+++ src/Library/OOBorrowedBook.java
@@ -30,4 +30,25 @@ public class OOBorrowedBook extends OOBook{
 		this.dueDate = dueDate;
 	}
 	
+	/**
+	 * Return all of book's properties as one string
+	 * Separator between props: "</sep/>
+	 * Order: 
+	 * @return
+	 */
+	private String getString(){
+		String sep = "</sep/>";
+		return (this.getTitle() + sep + this.getAuthor() + sep + this.getPublisher() + sep + this.getPublicationYear() + sep + this.getID()
+				+ sep + this.getGradeLevel() + sep +  this.getNumAvailable() + sep + this.getDueDate()).toString();
+	}
+	
+	/**
+	 * Give a byte array including all of book's properties. 
+	 * <br>
+	 * Separator : </sep/>
+	 * @return
+	 */
+	public byte[] getBytes(){
+		return (this.getString()).getBytes();
+	}
 }
