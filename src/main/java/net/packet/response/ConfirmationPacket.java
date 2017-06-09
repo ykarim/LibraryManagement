4b1079bb962e84c8abae835a286de7e2071aeb6b
ykarim <ykarim250@gmail.com>
2017-06-09 16:34:22 -0400

    Created ConfirmationPacket

---
+++ src/main/java/net/packet/response/ConfirmationPacket.java
@@ -0,0 +1,24 @@
+package net.packet.response;
+
+import net.packet.Packet;
+
+import java.util.List;
+
+public class ConfirmationPacket extends Packet {
+
+    private boolean confirmed;
+    private List<Object> confirmationData;
+
+    public ConfirmationPacket(boolean confirmed, List<Object> confirmationData) {
+        this.confirmed = confirmed;
+        this.confirmationData = confirmationData;
+    }
+
+    public boolean isConfirmed() {
+        return confirmed;
+    }
+
+    public List<Object> getConfirmationData() {
+        return confirmationData;
+    }
+}
