4cd90d1084d7ed0cc19cf99ec39c1993da0b4ee4
Yusuf Karim <ykarim250@gmail.com>
2017-06-10 11:40:12 -0400

    Log4j logger added

---
+++ src/main/java/log/Logger.java
@@ -0,0 +1,13 @@
+package log;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+
+public class Logger {
+
+    private static org.apache.logging.log4j.Logger logger = LogManager.getLogger();
+
+    public static void writeToLog(Level level, String message) {
+        logger.log(level, message);
+    }
+}
