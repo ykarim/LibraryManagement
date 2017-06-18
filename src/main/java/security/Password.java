8ba8414a9b7438214c87b033bb7c8c81928b7d6c
Yusuf Karim <ykarim250@gmail.com>
2017-06-18 00:53:53 -0400

    Created Password obj to store user passwords

---
+++ src/main/java/security/Password.java
@@ -0,0 +1,33 @@
+package security;
+
+public class Password {
+
+    private static int minLength, maxLength;
+    private String passwordHashed;
+
+    public Password(String password) {
+        passwordHashed = Encryption.encryptString(password);
+    }
+
+    public static void setMinLength(int minLength) {
+        Password.minLength = minLength;
+    }
+
+    public static void setMaxLength(int maxLength) {
+        Password.maxLength = maxLength;
+    }
+
+    public String getPasswordHashed() {
+        return passwordHashed;
+    }
+
+    public void generateRandomPassword() {
+        StringBuilder stringBuilder = new StringBuilder();
+        final int firstAsciiChar = 32;
+        final int lastAsciiChar = 126;
+        for (int index = 0; index < (int) (Math.random() * ((maxLength - minLength) + 1)) + minLength; index++) {
+            stringBuilder.append(Character.toString((char) ((int) (Math.random() * ((lastAsciiChar - firstAsciiChar) + 1)) + firstAsciiChar)));
+        }
+        passwordHashed = Encryption.encryptString(stringBuilder.toString());
+    }
+}
