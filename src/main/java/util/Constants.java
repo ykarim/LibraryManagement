1739d058e0cf0f76b1b26c44ad69b2c33c3a31b2
Yusuf Karim <ykarim250@gmail.com>
2017-06-20 01:11:30 -0400

    Constants class holds default variables

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -1,8 +1,11 @@
 package util;
 
+/**
+ * Holds default variables to be rewritten by properties file
+ */
 public class Constants {
 
-    public static int PORT = 5609;
-    public static int PASSWORD_MIN_LENGTH;
-    public static int PASSWORD_MAX_LENGTH;
+    public static int DEFAULT_PORT = 5609;
+    public static int DEFAULT_PASSWORD_MIN_LENGTH;
+    public static int DEFAULT_PASSWORD_MAX_LENGTH;
 }
